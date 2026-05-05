2d9031883e2fdd9ae5af3971212d2af47f49c7b1
vaishnavi maheshwari <117008958+vaishnavi192@users.noreply.github.com>
2026-05-05 15:00:35 +0530

    urls

--- src/Frontend/Components/api.js
+++ src/Frontend/Components/api.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-// const API_BASE_URL = "https://www.indian-ai.com";
-const API_BASE_URL = "http://localhost:8000";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8000";
 
 // Process Text Query (AI Response from Groq)
 export const processTextQuery = async (message, language_code) => {
